a04454f94b4bbe4feb3d2aad5b1bff37c9cc8a5b
manny1001 <Emmanuelmuya@gmail.com>
2024-06-03 09:25:32 +0100

    Created Password Hasher Helper Module

--- backend/src/services/adminService.js
+++ backend/src/services/adminService.js
@@ -1,17 +1,18 @@
 const Admin = require("../domain/entities/admin");
 const bcrypt = require("bcrypt");
+const { hashPassword } = require("../utils/passwordHasher");
 class AdminService {
   constructor(adminRepository) {
     this.adminRepository = adminRepository;
   }
-
   async createAdmin(adminDto) {
-    const admin = new Admin(
-      null,
-      adminDto.username,
-      await bcrypt.hash(adminDto.password, 10),
-      false
-    );
+    // Hash the password
+    const hashedPassword = await hashPassword(adminDto.password);
+    console.log("Hashed Password:", hashedPassword);
+    const admin = new Admin(null, adminDto.username, hashedPassword, false);
+
+    console.log(admin);
+
     return await this.adminRepository.save(admin);
   }
 
