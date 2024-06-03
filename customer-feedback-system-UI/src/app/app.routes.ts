afff1497b014037fb93808fec5ce32fe49f27d15
manny1001 <Emmanuelmuya@gmail.com>
2024-06-03 23:58:28 +0100

    Customer Feed Back Page and Routing

--- customer-feedback-system-UI/src/app/app.routes.ts
+++ customer-feedback-system-UI/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { LoginFormComponent } from './login-form/login-form.component';
+import { FeedbackFormComponent } from './feedback-form/feedback-form.component';
+import { AdminComponent } from './admin/admin.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "", pathMatch : "full",redirectTo : "feedback"},
+    {path : "login" , component : LoginFormComponent},
+    {path : "feedback" , component : FeedbackFormComponent},
+    {path : "admin" , component : AdminComponent},
+
+];
